5fc56b27b8b193bfc4a7b247fb4fa7a8d9e69844
uknow <yunhoknight@naver.com>
2023-04-12 16:43:12 +0900

    feat: view To-do Application

---
+++ src/main/java/com/codestates/TodoApp/Controller.java
@@ -0,0 +1,13 @@
+package com.codestates.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloworld() {
+        return "To-do Application!";
+    }
+}
